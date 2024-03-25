8df0711507406606360dd82b45282870362ee853
silvi923 <silviayuni475@gmail.com>
2024-03-25 11:23:40 +0700

    tugas 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan </h1>";
+        echo "Nama saya Silvi Ayuni
+        saya tinggal di Desa Tebang Kacang
+        Kegiatan yang saya lakukan adalah nonton anime dan konten idol K-Pop";
+    }
+}
